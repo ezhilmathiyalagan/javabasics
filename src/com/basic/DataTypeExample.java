d318328a30da77ee220cd315c318bd8b0df3c010
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-29 15:44:30 +0530

    DataTypeExample

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class DataTypeExample {
+public static void main(String[]args)
+{
+int count1=10;
+int count2=20;
+if(count1<count2)
+System.out.println("yes");
+else
+	System.out.println("no");
+}
+}
