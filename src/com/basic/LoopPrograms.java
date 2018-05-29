ea7ab15e32ff5784b39931083bfdab2c7fc1b555
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-29 17:02:36 +0530

    Mystery

---
+++ src/com/basic/LoopPrograms.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class LoopPrograms {
+	public static void main(String[]args)
+	{
+		int a=0;
+		while(a<6);
+		{
+			System.out.println("a= "+a);
+			a++;
+		}
+	}
+
+}
