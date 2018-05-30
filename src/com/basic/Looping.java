9a30d7d0f3ae43b3383b7fe338be623471585c29
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-30 12:08:11 +0530

    break statment

---
+++ src/com/basic/Looping.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Looping {
+	public static void main(String[]arg)
+	{
+		int x=1; //total
+		while(x<=10)
+		{
+			//total +=x;
+			++x;
+		}
+	}
+
+}
---
+++ src/com/basic/TransferPrograms.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class TransferPrograms {
+	public static void main(String[]arg)
+	{
+
+		int n = 2;
+		switch (n)
+		{
+		case 1:
+		System.out.println( "The number is 1" );
+		break;
+		case 2:
+		System.out.println( "The number is 2" );
+		break;
+		default:
+		System.out.println( "The number is not 1 or 2" );
+		break;
+		}
+
+
+
+	}
+
+}
