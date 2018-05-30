152d5790658766a1ef1c441a169cec66d0739ae5
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-30 12:39:20 +0530

    LoopingPrograms3

---
+++ src/com/basic/LoopingPrograms2.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class LoopingPrograms2 {
+	public static void main(String[]arg)
+	{
+		int i = 1;
+		while ( i <= 10 );
+		++i;
+		for (double k= 0.1; k != 1.0; k += 0.1 )
+		System.out.println( k );
+
+
+		
+	}
+
+}
