36af305724ca3382e1def91852203743b71f591a
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-31 18:24:15 +0530

    perfect number or not

--- src/com/basic/Perfect.java
+++ src/com/basic/Perfect.java
@@ -1,23 +1,24 @@
 package com.basic;
 
-public class Perfect {
-	public static void main(String[]arg) {
-		Perfect p1=new Perfect();
-		System.out.println("Is 7 is a perfect number"+p1.isperfect(7));
-	}
+public class Perfect {public static void main(String[]arg) {
+	Perfect p1=new Perfect();
+	System.out.println("Is 7 is a perfect number"+p1.isperfect(7));
+}
 public boolean isperfect(int number) {
-	int sum=0;
-	for(int i=1;i<=number/2;i++)
-	{
-		if(number%i==0) {
-			sum =sum+i;
-		}
-	}
-	if(sum==number) {
-		return true;
-	} 
-	else {
-		return false;
+int sum=0;
+for(int i=1;i<=number/2;i++)
+{
+	if(number%i==0) {
+		sum =sum+i;
 	}
 }
+if(sum==number) {
+	return true;
+} 
+else {
+	return false;
 }
+}
+}
+
+
