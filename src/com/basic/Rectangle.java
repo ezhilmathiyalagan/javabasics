2af4929669c24cb034a757c0a7bc88219961a25f
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-31 14:53:37 +0530

    Rectangle

---
+++ src/com/basic/Rectangle.java
@@ -0,0 +1,40 @@
+package com.basic;
+
+public class Rectangle {
+	float Length=1;
+	float Width=1;
+	public Rectangle() {
+		
+	}
+	public float getLength() {
+		return Length;
+	}
+	public void setLength(float length) {
+		if(Length>0.0)
+		Length = length;
+	}
+	public float getWidth() {
+		return Width;
+	}
+	public void setWidth(float width) 
+	{
+	if(Width<20.0)
+		Width = width;
+	}
+public double getPerimeter()
+{
+	return 2*(Length+Width);
+}
+public double getArea()
+{
+	return Length*Width;
+}
+public static void main(String[]arg)
+{
+	Rectangle r1=new Rectangle();
+	r1.setLength(1);
+	r1.setWidth(1);
+	System.out.println(r1.getPerimeter());
+	System.out.println(r1.getArea());
+	}
+}
